29ad51bdb5ad12a9158535b356bb42e1cf336878
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 22:05:03 +0200

    create reusbale container

---
+++ src/components/mainContainer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <main
+    class="bg-background text-foregound w-full h-screen flex flex-col justify-center items-center"
+  >
+    <slot />
+  </main>
+</template>
