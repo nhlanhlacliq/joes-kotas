baa2554eb4abfd52b61e9ed260f233ea1596724d
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 22:05:45 +0200

    create reusable component

---
+++ src/components/ui/labelComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <label :for="for" class="block text-sm font-medium mb-1 text-foreground/80">
+    <slot />
+  </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  for: string
+}>()
+</script>
