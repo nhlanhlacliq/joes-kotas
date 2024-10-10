281aaa26e37c46d52af07a25a916780fcaf36e81
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 19:46:12 +0200

    create darkmodetoggle

---
+++ src/components/darkModeToggle.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+import { useDarkModeStore } from '@/stores/dark-mode'
+
+const darkModeStore = useDarkModeStore()
+
+const darkModeButtonClasses =
+  'bg-foreground rounded-full p-2 border-border border-2 shadow-lg hover:scale-125 transition-all duration-300 cursor-pointer'
+
+watch(
+  () => darkModeStore.isDarkMode,
+  () => {
+    document.documentElement.classList.toggle('dark', !darkModeStore.isDarkMode)
+  }
+)
+</script>
+
+<template>
+  <div class="absolute bottom-10 right-10">
+    <button
+      v-if="darkModeStore.isDarkMode"
+      @click="darkModeStore.toggleDarkMode"
+      :class="darkModeButtonClasses"
+    >
+      🌜
+    </button>
+    <button v-else @click="darkModeStore.toggleDarkMode" :class="darkModeButtonClasses">☀️</button>
+  </div>
+</template>
