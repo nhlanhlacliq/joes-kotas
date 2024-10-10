bd6a5fd1ececa6bda2f2a451db8ffd99e8cd3ef1
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 22:17:54 +0200

    create dashboard view (WIP)

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ButtonComponent from '../components/ui/buttonComponent.vue'
+import MainContainer from '@/components/mainContainer.vue'
+</script>
+
+<template>
+  <MainContainer>
+    <h1 class="text-4xl font-bold text-foreground">Joe's Kotas Dashboard 🍞</h1>
+    <router-link to="/">
+      <ButtonComponent class="w-32"> Logout </ButtonComponent>
+    </router-link>
+  </MainContainer>
+</template>
