7b7164e858e04e344a59b54002b325f46ec49d20
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 01:46:02 +0200

    add route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import DarkModeToggle from './components/darkModeToggle.vue'
+import { useDarkModeStore } from './stores/dark-mode'
+
+const darkModeStore = useDarkModeStore()
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <transition :name="darkModeStore.isDarkMode ? 'slide' : 'dark'" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </RouterView>
   <DarkModeToggle />
 </template>
 
