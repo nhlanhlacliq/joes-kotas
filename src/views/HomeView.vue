d2665a1184d8c39c7de5329a2c9a0b34ffc2ac5a
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 18:43:50 +0200

    create basic home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonComponent from '../components/ui/buttonComponent.vue'
+</script>
 
 <template>
   <main
-    class="bg-gradient-to-bl from-slate-900 to-black text-white w-full h-screen flex flex-col justify-center items-center"
+    class="bg-background text-foregound w-full h-screen flex flex-col justify-center items-center"
   >
-    <h1 class="text-4xl font-bold">Welcome to Joe's Kotas 🍞</h1>
+    <div class="flex flex-col gap-6 items-center">
+      <h1 class="text-4xl font-bold text-foreground">Welcome to Joe's Kotas 🍞</h1>
+      <p class="text-xl text-foreground/50 text-balance">
+        The ultimate dashboard for managing your inventory
+      </p>
+      <div>
+        <ButtonComponent variant="outline">Welcome</ButtonComponent>
+      </div>
+    </div>
   </main>
 </template>
