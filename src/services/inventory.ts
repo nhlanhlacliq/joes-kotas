ae9a29fef1e1fc65451bc54c91839dd675b6f50d
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 12:29:15 +0200

    rename - cleanup

--- src/services/inventory.ts
+++ src/services/inventory.ts
@@ -1,14 +1,15 @@
 import axios from '@/api/axios'
+import type { InventoryItemInputSchema } from '@/schemas/inventory'
 
 export const fetchInventory = async () => {
   return await axios.get('/inventory')
 }
 
-export const createInventoryItem = async (item) => {
+export const createInventoryItem = async (item: InventoryItemInputSchema) => {
   return await axios.post('/inventory', item)
 }
 
-export const updateInventoryItem = async (id: number, item) => {
+export const updateInventoryItem = async (id: number, item: InventoryItemInputSchema) => {
   return await axios.put(`/inventory/${id}`, item)
 }
 
