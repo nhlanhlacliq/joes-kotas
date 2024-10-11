5ffcc90fae70c3cfd0060e1049ad2086d26e0f51
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 21:14:40 +0200

    refactor

---
+++ src/components/drowdownMenuItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  label: string
+}>()
+</script>
+
+<template lang="">
+  <div class="p-3 px-2 flex gap-3 cursor-pointer hover:bg-border/25">
+    <div>{{ icon }}</div>
+    <div>{{ label }}</div>
+  </div>
+</template>
