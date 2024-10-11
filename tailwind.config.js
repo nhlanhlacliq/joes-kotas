6c98dba78df0293483c1fbf1f357ad57026a02c2
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 16:29:51 +0200

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,ts,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{vue,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        'background': 'hsl(var(--background))',
-        'foreground': 'hsl(var(--foreground))',
-        'primary': 'hsl(var(--primary))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: 'hsl(var(--primary))',
         'primary-foreground': 'hsl(var(--primary-foreground))',
-        'secondary': 'hsl(var(--secondary))',
+        secondary: 'hsl(var(--secondary))',
         'secondary-foreground': 'hsl(var(--secondary-foreground))',
-        'destructive': 'hsl(var(--destructive))',
+        destructive: 'hsl(var(--destructive))',
         'destructive-foreground': 'hsl(var(--destructive-foreground))',
+        border: 'hsl(var(--border))'
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
