00bcf0c5ea99ffbd82b3a0cd1f0798c1f112522d
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 16:30:40 +0200

    create loading component

---
+++ src/components/loadingComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(1)
+
+setInterval(() => {
+  count.value === 4 ? (count.value = 1) : count.value++
+}, 100)
+</script>
+
+<template>
+  <p v-if="count === 1" class="opacity-85 -translate-x-[0px]">Loading</p>
+  <p v-else-if="count === 2" class="translate-y-[0px]">Loading.</p>
+  <p v-else-if="count === 3" class="opacity-85 translate-x-[1px]">Loading..</p>
+  <p v-else-if="count === 4" class="-translate-y-[1px]">Loading...</p>
+</template>
