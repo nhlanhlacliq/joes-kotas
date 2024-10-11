6cea5466e5f1fc9eadb2b5e24aa6bb97a60fdfc1
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 12:30:38 +0200

    create schema

---
+++ src/schemas/inventory.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const inventoryItemCompleteSchema = z.object({
+  id: z.number(),
+  name: z.string().min(1, { message: 'Name is required' }),
+  count: z.number().int().min(0, { message: 'Count cannot be negative' }),
+  isAvailable: z.boolean(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime()
+})
+
+const inventoryItemInputSchema = inventoryItemCompleteSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true
+})
+export type InventoryItemInputSchema = z.infer<typeof inventoryItemInputSchema>
