626dcd0e734f74e1eec978456c5cd72a44acab91
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-11 04:03:42 +0200

    add register fucntionality to form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,28 +4,43 @@ import Button from '@/components/ui/buttonComponent.vue' // Using the simplified
 import Card from '@/components/ui/cardComponent.vue'
 import Input from '@/components/ui/inputComponent.vue'
 import Label from '@/components/ui/labelComponent.vue'
+import { useAuthStore } from '@/stores/auth'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+const authStore = useAuthStore()
 
 const passwordShow = ref(false)
 
 const email = ref('')
 const username = ref('')
 const password = ref('')
+const errorMessage = ref('')
 
-function handleLogin() {
-  console.log('Email:', email.value)
-  console.log('Username:', email.value)
-  console.log('Password:', password.value)
+async function handleRegister() {
+  try {
+    await authStore.register({
+      email: email.value,
+      username: username.value,
+      password: password.value
+    })
+    router.push('/dashboard')
+  } catch (error) {
+    console.error('Registration failed:', error)
+    if (error.response && error.response.data && error.response.data.error) {
+      errorMessage.value = error.response.data.error
+    } else {
+      errorMessage.value = 'An error occurred during registration.'
+    }
+  }
 }
 </script>
 
 <template>
   <MainContainer>
     <Card class="w-full max-w-sm p-6 shadow-sm">
-      <form class="flex flex-col gap-4" @submit.prevent="handleLogin">
+      <form class="flex flex-col gap-4" @submit.prevent="handleRegister">
         <div>
           <h2 class="text-2xl font-semibold pb-2">Register</h2>
           <p class="text-sm text-foreground/50 mb-4">
@@ -60,10 +75,11 @@ function handleLogin() {
               </div>
             </div>
           </div>
-          <Button class="w-full mt-4" type="submit"> Sign in </Button>
+          <Button class="w-full mt-4" type="submit"> Register </Button>
           <Button class="w-full" variant="outline" @click="router.push('/')"> Cancel </Button>
         </div>
       </form>
+      <div v-if="errorMessage" class="text-red-500 text-sm">{{ errorMessage }}</div>
     </Card>
   </MainContainer>
 </template>
