7353056fac76cf388cc4e4c2e6323f27849fafec
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-12 02:52:40 +0200

    remove unused vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,8 @@ export default {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: 'hsl(var(--primary))',
-        'primary-foreground': 'hsl(var(--primary-foreground))',
         secondary: 'hsl(var(--secondary))',
-        'secondary-foreground': 'hsl(var(--secondary-foreground))',
         destructive: 'hsl(var(--destructive))',
-        'destructive-foreground': 'hsl(var(--destructive-foreground))',
         border: 'hsl(var(--border))'
       },
       borderRadius: {
