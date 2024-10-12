39f2624fa2be99a17573c9916904da58ff662c87
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-12 02:53:32 +0200

    add helper text

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -15,17 +15,21 @@ function handleLogout() {
 <template>
   <!-- Sidebar -->
   <div
-    class="flex flex-col justify-between w-full bg-foreground dark:bg-foreground/10 text-background dark:text-foreground max-w-56 p-6"
+    class="flex flex-col justify-between w-full bg-foreground dark:bg-foreground/10 text-background dark:text-foreground max-w-60 p-6"
   >
     <div class="space-y-6">
       <h2 class="text-2xl font-bold">🍞 Joes Kotas</h2>
       <p class="">Hello, {name}</p>
       <p class="opacity-50">
-        lorem ipsum etae et dolor sit amet consectetur adipiscing elit sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+        Use the search bar to quickly find items by name.
+        <br />
+        <br />
+        You can also sort the table by clicking on the column headers.
+        <br />
+        <br />
+        Click on the <span class="text-3xl -mb-10"> ... </span> button to edit or delete an item.
       </p>
     </div>
-    <Button @click="handleLogout"> 🚪 Logout </Button>
+    <Button @click="handleLogout" variant="destructive"> 🚪 Logout </Button>
   </div>
 </template>
