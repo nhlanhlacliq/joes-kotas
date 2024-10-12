6f2668c82b6b98d8b762e5a00bfd50881bf3452b
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-12 04:14:09 +0200

    refactor

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,13 +1,5 @@
 import { z } from 'zod'
-
-export const userSchema = z.object({
-  id: z.number(),
-  email: z.string().email({ message: 'Invalid email address' }),
-  username: z.string().min(2, { message: 'Username must be at least 2 characters long' }),
-  password: z.string().min(8, { message: 'Password is required (Minimum 8 characters)' }),
-  createdAt: z.string().datetime(),
-  updatedAt: z.string().datetime()
-})
+import { userSchema } from './user'
 
 // User registration
 export const registerUserSchema = userSchema.omit({ id: true, createdAt: true, updatedAt: true })
