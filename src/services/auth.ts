771ceaa9d796d0149c7158e78d885078082d70d9
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-12 04:14:28 +0200

    refactor

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -8,11 +8,3 @@ export const register = async (credentials: RegisterUserSchema) => {
 export const login = async (credentials: LoginUserSchema) => {
   return await axios.post('/auth/login', credentials)
 }
-
-export const getCurrentUser = async () => {
-  return await axios.get('/auth/me')
-}
-
-export const getAllUsers = async () => {
-  return await axios.get('/users')
-}
